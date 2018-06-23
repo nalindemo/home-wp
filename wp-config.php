20b9afd815898b9230a44282be16739550c507dc
nalin.gupta <nalin.gupta@iproperty.com>
2018-06-24 01:28:41 +0800

    Added all sample file for wordpress

---
+++ wp-config.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'wordpress');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'wordpress');
+
+/** MySQL hostname */
+define('DB_HOST', 'db:3306');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'fd953d19cb768804287ec166374f34d7ba50a66d');
+define('SECURE_AUTH_KEY',  'b3aac969e710df00e197178fd3d7db1d80cc5c9e');
+define('LOGGED_IN_KEY',    '73ecec7d71f8f2b92574fa196901bada42eac67f');
+define('NONCE_KEY',        '363ea781c26739491bd4821866dd9564d13f5e15');
+define('AUTH_SALT',        '3fb6e0dcdc0f1151ba55a7f5da9666f7cb6a4d86');
+define('SECURE_AUTH_SALT', 'd5b3a96b4db43f7495506452ad35b9c1ec2d17b1');
+define('LOGGED_IN_SALT',   'd8b139c02bcd147f16f227d1c510e0a4729db1e0');
+define('NONCE_SALT',       '404dcc9ab02ed2d4fbaeb92bc12d923fb4c064f2');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/single-page-maker/readme.txt
@@ -0,0 +1,42 @@
+=== Single Page maker ===
+
+Contributors: Blogwings
+Tags: right-sidebar, custom-menu, custom-logo, editor-style, featured-images, translation-ready, flexible-header, sticky-post, theme-options, portfolio
+Requires at least: 4.0.5
+Tested up to: 4.9.2
+Stable tag: 1.0.1
+
+== Theme License & Copyright ==
+ Single Page maker is distributed under the terms of the GNU GPL
+Single Page maker Copyright 2018  Single Page maker, blogwings.com
+License : GPL License
+License URL: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+"Single Page maker theme One Page Business WordPress Theme" By Blogwings.
+
+== Parent Theme License & Copyright ==
+
+Single page maker WordPress Theme is child theme of featuredlite WordPress Theme, 
+Featured Lite WordPress theme, copyright (c) 2018 themehunk.com
+Featured Lite Theme is distributed under the terms of the GNU GPL3
+
+== Description ==
+Single page maker theme is specially designed for corporate/business websites. Theme is user friendly and its instant set up process help you to create professional website for Business (Agency, Interior, Florist, Transport, Construction etc), Shop / eCommerce, Personal, Blog, Photography. This responsive theme comes with live customizer, Full body background slider and background image option, Translation ready, Header layouts (Default, Center and Split), Widget based home page sections with unlimited items capability, Unlimited color option, Advance Section styling, Professionally designed sections (Ribbon, Services, About Us, Call to action, Team, Shop (WooCommerce), Testimonial, Latest News, Contact Us (With call to, Mail to, Address and Map). Theme contain seven widget area (Four for footer, Two for sidebar (default pages) and one for shop page). Theme is compatible with WooCommerce, bbPress, Lead form builder, Contactform7 and with many other WP plugin.
+
+License for images:
+1. Image Name: main.jpeg 
+Resource link: https://www.pexels.com/photo/adult-books-business-coffee-374016/
+Licensed under the CCO license.
+License link : https://www.pexels.com/photo-license/
+
+== FontAwesome License ==
+License: SIL OFL 1.1
+License URl for Font : http://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL
+
+== 1.0.0  =
+* Initial release
+
+Once again, thank you so much for trying the Single page maker WordPress Theme. 
+As we said at the beginning, we will be glad to help you. 
+If you have any questions related to this theme then do let us know & we will try our best to assist. 
+If you have any general questions related to the themes on blogwings, we would recommend you to visit the blogwings site and mail us to ask your queries.
